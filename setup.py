428af2825294efb09b7ada44f03c74b358ebad28
root <root@localhost.localdomain>
2025-10-30 19:43:56 +0000

    feat: Initial release v2.0.0 - Indonesian legal document analysis

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="versalaw2",
+    version="2.0.0",
+    packages=["versalaw2"],
+    author="Creator Source",
+    description="Legal document analysis library",
+    python_requires=">=3.7",
+)
---
+++ versalaw2/__init__.py
@@ -0,0 +1,12 @@
+from .core import analyze_contract, extract_clauses, normalize_text, validate_legal_format
+
+__version__ = "2.0.0"
+__author__ = "Creator Source"
+__email__ = "creator@source.com"
+
+__all__ = [
+    'analyze_contract',
+    'extract_clauses',
+    'normalize_text', 
+    'validate_legal_format'
+]
